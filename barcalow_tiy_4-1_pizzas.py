e01e82a1f2df93fadde3e7c9352969ea5bc6b19e
Henry Barcalow <118207101+HB-the-Pencil@users.noreply.github.com>
2024-09-05 10:23:07 -0400

    Upload TIYs 4-1 and 4-2

---
+++ barcalow_tiy_4-1_pizzas.py
@@ -0,0 +1,8 @@
+# List some types of pizza.
+pizzas = ["supreme", "sausage", "hawaiian", "pepperoni"]
+
+# I was planning to remove Hawaiian as a joke, but it's perfectly good pizza.
+for pizza in pizzas:
+    print(f"{pizza.title()} pizza is delicious.")  # Title case because it's the start of the sentence.
+
+print("Supreme pizza is my favorite kind. (I will die on that hill.)")
---
+++ barcalow_tiy_4-2_animals.py
@@ -0,0 +1,8 @@
+# Can you guess what these animals all have in common?
+animals = ["snake", "bird", "dolphin"]
+
+for animal in animals:
+    print(f"A {animal} is used as the logo for a programming language.")
+
+# All of these animals are on the programming languages graphic outside our classroom.
+print("Python's logo uses a snake, Swift's uses a bird, and mySQL's uses a dolphin.")
